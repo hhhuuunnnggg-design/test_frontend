9aae5caca8f526891003ed56592b4cf2c81cd42c
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2025-03-20 15:54:43 +0700

    ve giao dien http://localhost:8080/api/employee (13)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/dashboard/dashboard.js";
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import "./dashboard.css";
+
 const Dashboard = () => {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/employee");
+        const data = await response.json();
+        setEmployees(data);
+      } catch (error) {
+        console.log("error fetching employees", error);
+      }
+    };
+    fetchEmployees();
+  }, []);
   return (
-    <div>
-      <h1>Dashboard compomen heheh 123</h1>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1 className="text-center">Employee</h1>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Department</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee, index) => (
+                <tr key={employee.id}>
+                  <td>{index + 1}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.phone}</td>
+                  <td>{employee.department}</td>
+                  <td>
+                    <Button variant="outline-secondary" className="me-2">
+                      Update
+                    </Button>
+                    <Button variant="outline-danger">Delete</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Dashboard;
