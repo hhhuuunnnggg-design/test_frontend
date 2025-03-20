6b0c6687c1a869b3244f9cfe6daff8f7980119dd
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2025-03-20 15:00:15 +0700

    giao dien push user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./pages/header/header";
+import Dashboard from "./pages/dashboard/dashboard.js";
+import PostUser from "./pages/employee/postUser.js";
+import Header from "./pages/header/header.js";
+import NoMatch123 from "./pages/noMatch/noMatch.js"; // Viết hoa đúng với tên file
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />;
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/employees" element={<PostUser />} />
+        <Route path="*" element={<NoMatch123 />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/noMatch/noMatch.js
@@ -0,0 +1,8 @@
+const Nomath = () => {
+  return (
+    <div>
+      <h1>404 Page</h1>
+    </div>
+  );
+};
+export default Nomath;
